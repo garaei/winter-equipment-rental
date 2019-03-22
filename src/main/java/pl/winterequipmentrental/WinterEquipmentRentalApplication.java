823b1bb57b8c228b54aacb9710d0cb4a5cbc42bb
Jarosław Dziewulski <jaroslaw.dzie96@gmail.com>
2019-03-22 23:14:16 +0100

    Initial commit

---
+++ src/main/java/pl/winterequipmentrental/WinterEquipmentRentalApplication.java
@@ -0,0 +1,13 @@
+package pl.winterequipmentrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterEquipmentRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinterEquipmentRentalApplication.class, args);
+    }
+
+}
