20ec186531ee2c55c60a2c2f7880c43083ce4bc6
Jarosław Dziewulski <jaroslaw.dzie96@gmail.com>
2019-04-17 12:56:08 +0200

    Replace ResponeEntity to static method

--- src/main/java/pl/winterequipmentrental/controllers/PositionController.java
+++ src/main/java/pl/winterequipmentrental/controllers/PositionController.java
@@ -1,12 +1,14 @@
 package pl.winterequipmentrental.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.winterequipmentrental.model.person.employee.Position;
 import pl.winterequipmentrental.services.PositionService;
 
+import java.net.URI;
+import java.util.Optional;
 import java.util.Set;
 
 @RestController
@@ -21,58 +23,62 @@ public class PositionController {
     }
 
     @PostMapping
-    public ResponseEntity<Position> createPosition(@RequestBody Position position) {
-        return new ResponseEntity<>(positionService.save(position), HttpStatus.CREATED);
+    public ResponseEntity<Position> createPosition(@RequestBody final Position position) {
+        Position result = positionService.save(position);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(result.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Position> findPositionById(@PathVariable long id) {
-        return new ResponseEntity<>(
-                positionService.findById(id).orElse(null),
-                HttpStatus.OK);
+    public ResponseEntity<Position> findPositionById(@PathVariable final long id) {
+        Optional<Position> position = positionService.findById(id);
+        if (position.isPresent())
+            return ResponseEntity.ok(position.get());
+        return ResponseEntity.badRequest().build();
     }
 
-    @GetMapping("/name")
-    public ResponseEntity<Position> findPositionByName(@RequestParam String name) {
-        return new ResponseEntity<>(
-                positionService.findByName(name).orElse(null),
-                HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<Position> findPositionByName(@RequestParam final String name) {
+        Optional<Position> position = positionService.findByName(name);
+        if (position.isPresent())
+            return ResponseEntity.ok(position.get());
+        return ResponseEntity.badRequest().build();
     }
 
     @GetMapping
     public ResponseEntity<Set<Position>> findAllPosition() {
-        return new ResponseEntity<>(positionService.findAll(), HttpStatus.OK);
+        return ResponseEntity.ok(positionService.findAll());
     }
 
     @GetMapping("/names")
     public ResponseEntity<Set<String>> findAllPositionName() {
-        return new ResponseEntity<>(positionService.findAllPositionNames(), HttpStatus.OK);
+        return ResponseEntity.ok(positionService.findAllPositionNames());
     }
 
     @DeleteMapping("/{id}")
-    public void removePositionById(@PathVariable long id) {
+    public void removePositionById(@PathVariable final long id) {
         positionService.deleteById(id);
     }
 
     @DeleteMapping("/{name}")
-    public void removePositionByName(@PathVariable String name) {
+    public void removePositionByName(@PathVariable final String name) {
         positionService.deleteByName(name);
     }
 
     @PutMapping
-    public ResponseEntity<Position> updatePosition(@PathVariable Position position) {
+    public ResponseEntity<Position> updatePosition(@PathVariable final Position position) {
         positionService.update(position);
-        return new ResponseEntity<>(
-                positionService.findById(position.getId()).orElse(null),
-                HttpStatus.OK);
+        return ResponseEntity.ok(positionService.findById(position.getId()).orElse(null));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Position> updateDescritpionPosition(@PathVariable long id,
-                                                              @RequestParam String description) {
+    public ResponseEntity<Position> updateDescritpionPosition(@PathVariable final long id,
+                                                              @RequestParam final String description) {
         positionService.updateDescription(id, description);
-        return new ResponseEntity<>(
-                positionService.findById(id).orElse(null),
-                HttpStatus.OK);
+        return ResponseEntity.ok(positionService.findById(id).orElse(null));
     }
 }
